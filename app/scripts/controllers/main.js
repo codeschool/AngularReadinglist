7562eccd761f190564342572e64d4d9f8b552945
David Rogers <david@orlandophp.org>
2014-04-08 16:14:19 -0400

    Generated: yo angular (http://yeoman.io/)

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularReadingListApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
